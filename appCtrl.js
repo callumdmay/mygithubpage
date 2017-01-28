d584624c19c44031f3f85d78f4efd79102e43936
Callum May <callumdmay@gmail.com>
2017-01-28 13:24:38 -0500

    Updates

--- appCtrl.js
+++ appCtrl.js
@@ -1,43 +1,46 @@
 angular.module('myApp.controllers').
 controller('appCtrl', function($scope, $location) {
 
-    $scope.animateElementInDown = function($el) {
-        $el.removeClass('not-visible');
-        $el.addClass('animated fadeInDown'); // this example leverages animate.css classes
-    };
+  $scope.isActive = function(viewLocation) {
+    return viewLocation === $location.path();
+  };
 
-    $scope.animateElementInUp = function($el) {
-        $el.removeClass('not-visible');
-        $el.addClass('animated fadeInUp'); // this example leverages animate.css classes
-    };
+  $scope.isCollapsed = true;
 
-    $scope.animateElementFadeIn = function($el) {
-        $el.removeClass('not-visible');
-        $el.addClass('animated fadeIn'); // this example leverages animate.css classes
-    };
+  $scope.animateElementInDown = function($el) {
+    $el.removeClass('not-visible');
+    $el.addClass('animated fadeInDown'); // this example leverages animate.css classes
+  };
+
+  $scope.animateElementInUp = function($el) {
+    $el.removeClass('not-visible');
+    $el.addClass('animated fadeInUp'); // this example leverages animate.css classes
+  };
+
+  $scope.animateElementFadeIn = function($el) {
+    $el.removeClass('not-visible');
+    $el.addClass('animated fadeIn'); // this example leverages animate.css classes
+  };
 
 
-    $scope.animateElementBounceInLeft = function($el) {
-        $el.removeClass('not-visible');
-        $el.addClass('animated bounceInLeft'); // this example leverages animate.css classes
-    };
+  $scope.animateElementBounceInLeft = function($el) {
+    $el.removeClass('not-visible');
+    $el.addClass('animated bounceInLeft'); // this example leverages animate.css classes
+  };
 
-    $scope.animateElementBounceInRight = function($el) {
-        $el.removeClass('not-visible');
-        $el.addClass('animated bounceInRight'); // this example leverages animate.css classes
-    };
+  $scope.animateElementBounceInRight = function($el) {
+    $el.removeClass('not-visible');
+    $el.addClass('animated bounceInRight'); // this example leverages animate.css classes
+  };
 
-    $scope.animateElementFadeInLeft = function($el) {
-        $el.removeClass('not-visible');
-        $el.addClass('animated fadeInLeft'); // this example leverages animate.css classes
-    };
+  $scope.animateElementFadeInLeft = function($el) {
+    $el.removeClass('not-visible');
+    $el.addClass('animated fadeInLeft'); // this example leverages animate.css classes
+  };
 
-    $scope.animateElementFadeInRight = function($el) {
-        $el.removeClass('not-visible');
-        $el.addClass('animated fadeInRight'); // this example leverages animate.css classes
-    };
+  $scope.animateElementFadeInRight = function($el) {
+    $el.removeClass('not-visible');
+    $el.addClass('animated fadeInRight'); // this example leverages animate.css classes
+  };
 
-    $scope.isActive = function (viewLocation) {
-        return viewLocation === $location.path();
-    };
 });
